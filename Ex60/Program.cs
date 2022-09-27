dd76af2d85bb1fbaa9b41f0f7985959b6bc48ed3
Katya <katvas785@gmail.com>
2022-09-27 18:24:08 +0300

    Ex60

---
+++ Ex60/Program.cs
@@ -0,0 +1,37 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
+int[,,] array = new int[m, n, a];
+FillArray(array);
+PrintIndex(array);
+
+void PrintIndex(int[,,] array1)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+void FillArray(int[,,] array1)
+{
+    int count = 10;
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            for (int k = 0; k < array1.GetLength(2); k++)
+            {
+                array1[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
